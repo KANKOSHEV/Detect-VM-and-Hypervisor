fc65421673b5965aec682a16441d09ce3a75343a
LazyAhora <84189666+LazyAhora@users.noreply.github.com>
2021-06-06 15:06:14 +0300

    Update main.cpp

--- DetectVirtualMachineFirstLib/main.cpp
+++ DetectVirtualMachineFirstLib/main.cpp
@@ -13,10 +13,11 @@ int main() {
 	std::cout << "Rdtscp  + cpuid ->\t" << DetectHyp::Rdtscp() << '\n';
 	std::cout << "Rdtsc  with GetHeap & CloseHandle ->\t" << DetectHyp::RdtscHeap() << '\n';
 	std::cout << "SYSTEM_HYPERVISOR_DETAIL_INFORMATION ->\t" << DetectHyp::SysHypervInform() << '\n';
-	std::cout << "Exit VM with xgetbv ->\t" << DetectHyp::VMExit_xgetbv() << '\n';
-	std::cout << "Check virt sidt ->\t" << DetectHyp::SidtExcept() << '\n';
-	std::cout << "Check virt indv ->\t" << DetectHyp::CheckIndv() << '\n';
-	std::cout << "Check virt LBR ->\t" << DetectHyp::LBRBadVirtCheck() << '\n';
+	//work only ring 0
+	//std::cout << "Exit VM with xgetbv ->\t" << DetectHyp::VMExit_xgetbv() << '\n';
+	//std::cout << "Check virt sidt ->\t" << DetectHyp::SidtExcept() << '\n';
+	//std::cout << "Check virt indv ->\t" << DetectHyp::CheckIndv() << '\n';
+	//std::cout << "Check virt LBR ->\t" << DetectHyp::LBRBadVirtCheck() << '\n';
 	std::cout << "Detect hyp by cpuid & name ->\t" << DetectHyp::CpuidbyName() << '\n';
 	std::cout << "Cpuid is hyperv ->\t" << DetectHyp::CpuidIsHyperv() << '\n';
 	std::cout << "Mistake hyp cpuid ->\t" << DetectHyp::SehCpuid() << '\n';
